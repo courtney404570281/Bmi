7b633b6c0a87e83ab143087a42bd67f541696c50
Teresa Liang <teresa110213@gmail.com>
2018-09-22 10:15:30 +0800

    add bmi functions

--- app/src/main/java/teresa/com/bmi/MainActivity.java
+++ app/src/main/java/teresa/com/bmi/MainActivity.java
@@ -1,13 +1,68 @@
 package teresa.com.bmi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
+    private TextView result;
+    private Button help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+
+    }
+
+    private void findViews() {
+        edWeight = findViewById(R.id.edt_weight);
+        edHeight = findViewById(R.id.edt_height);
+        result = findViewById(R.id.txt_result);
+        help = findViewById(R.id.btn_help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Help")
+                        .setMessage("The body mass index (BMI) or Quetelet index is a value derived from the mass (weight) and height of an individual.")
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
+        });
+    }
+
+
+    public void bmi(View view){
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight / (height * height);
+        Log.d("MainActivity", "BMI: " + bmi);
+        Toast.makeText(this , "Your BMI is : " + bmi, Toast.LENGTH_LONG).show();
+        result.setText("Your BMI is :" + bmi);
+        new AlertDialog.Builder(this)
+                .setTitle("BMI")
+                .setMessage("Your BMI is :" + bmi)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        edHeight.setText("");
+                        edWeight.setText("");
+                    }
+                })
+                .show();
     }
 }
